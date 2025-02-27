d1aa93b995eaa950ed60a213d3fd5ed20ba2e483
MamaliFT <mohammad.ali.208777@gmail.com>
2025-02-27 12:28:31 +0330

    Create squere.py
    salam

---
+++ squere.py
@@ -0,0 +1,6 @@
+lenght=int(input("enter the lneght of the  Rectangle"))
+width= int(input("enter the width if the Rectangle"))
+for i in  range(lenght) :
+    for a in range(width) :
+        print("*", end="")
+    print()
